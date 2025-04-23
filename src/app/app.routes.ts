9fadbd08affeb461da7d3e68acab3f21f6c5a692
LanaNew <l4n.steen@gmail.com>
2025-04-23 14:18:02 +0400

    routing changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { BookingComponent } from './pages/booking/booking.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HeaderComponent, title: 'home' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, title: 'home' },
     { path: 'search', component: SearchComponent,title : 'search' },
     { path: 'booking/:id', component: BookingComponent,title: 'booking' },
     { path: '**', redirectTo: '', pathMatch: 'full' }
